68526abbba7ed0d63390dd0bdc5a6e62769f7e6a
Alexander Tuktarov <ATuktarov@web3tech.ru>
2021-03-10 11:12:21 +0300

    up sql

--- migrations/2021-03-02-090539_init/down.sql
+++ migrations/2021-03-02-090539_init/down.sql
@@ -1,4 +1,4 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS blocks_microblocks;
+DROP TABLE IF EXISTS associated_addresses;
 DROP TABLE IF EXISTS transactions;
-DROP TABLE IF EXISTS recipients;
+DROP TABLE IF EXISTS blocks_microblocks;
