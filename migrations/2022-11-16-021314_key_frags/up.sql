8eb11a5dcb43213cc8110490b983dc9f8830d854
Alex Kordys <akordys@web3tech.ru>
2023-03-13 11:49:55 +0300

    Indexes on key fragment columns

--- migrations/2022-11-16-021314_key_frags/up.sql
+++ migrations/2022-11-16-021314_key_frags/up.sql
@@ -6,12 +6,3 @@ ALTER TABLE data_entries ADD COLUMN key_frag_4 VARCHAR DEFAULT NULL;
 ALTER TABLE data_entries ADD COLUMN key_frag_5 VARCHAR DEFAULT NULL;
 ALTER TABLE data_entries ADD COLUMN key_frag_6 VARCHAR DEFAULT NULL;
 ALTER TABLE data_entries ADD COLUMN key_frag_7 VARCHAR DEFAULT NULL;
-
--- CREATE INDEX data_entries_key_frag_0_idx ON data_entries(key_frag_0);
--- CREATE INDEX data_entries_key_frag_1_idx ON data_entries(key_frag_1);
--- CREATE INDEX data_entries_key_frag_2_idx ON data_entries(key_frag_2);
--- CREATE INDEX data_entries_key_frag_3_idx ON data_entries(key_frag_3);
--- CREATE INDEX data_entries_key_frag_4_idx ON data_entries(key_frag_4);
--- CREATE INDEX data_entries_key_frag_5_idx ON data_entries(key_frag_5);
--- CREATE INDEX data_entries_key_frag_6_idx ON data_entries(key_frag_6);
--- CREATE INDEX data_entries_key_frag_7_idx ON data_entries(key_frag_7);
