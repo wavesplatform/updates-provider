fff555dd300e5e1f363bc2b95747c016b4ce4c00
Dmitry Shuranov <dvshur@gmail.com>
2023-07-18 12:16:45 +0300

    handle non-existent assets

--- src/asset_info.rs
+++ src/asset_info.rs
@@ -1,11 +1,12 @@
 //! Asset info loading from asset-service
 
 use crate::error::Error;
+use itertools::Itertools;
 use std::{collections::HashMap, sync::RwLock, time::Duration};
 use tokio::time::sleep;
 use wavesexchange_apis::{
     assets::dto::{AssetInfo, OutputFormat},
-    ApiResult, AssetsService, HttpClient,
+    AssetsService, HttpClient,
 };
 use wavesexchange_log::{debug, error, warn};
 use wx_topic::ExchangePair;
@@ -32,26 +33,28 @@ impl AssetStorage {
 
         let mut attempt = 0;
         let mut retry_delay = 30;
+        let mut last_error: Option<Error> = None;
         while retry_delay < 600 {
             if attempt > 0 {
                 warn!("Attempt #{} to load decimals for pair {:?}", attempt, pair);
             }
-            let res = self.load(pair).await;
-            if res.is_ok() {
-                return Ok(());
-            }
-            if let Err(e) = res {
-                debug!("asset-service error, sleeping {}s: {}", retry_delay, e);
-                sleep(Duration::from_secs(retry_delay)).await;
-                retry_delay *= 2;
-                attempt += 1;
+
+            match self.load(pair).await {
+                Ok(..) => return Ok(()),
+                Err(e) => {
+                    debug!("asset-service error, sleeping {}s: {}", retry_delay, e);
+                    last_error = Some(e);
+                    attempt += 1;
+                    sleep(Duration::from_secs(retry_delay)).await;
+                    retry_delay *= 2;
+                }
             }
         }
 
-        Err(Error::AssetServiceClientError(format!(
-            "failed to query decimals from asset-service for assets {} and {}",
-            pair.amount_asset, pair.price_asset
-        )))
+        match last_error {
+            Some(e) => Err(e),
+            None => Ok(()),
+        }
     }
 
     fn is_loaded(&self, pair: &ExchangePair) -> bool {
@@ -59,30 +62,49 @@ impl AssetStorage {
         assets.contains_key(&pair.amount_asset) && assets.contains_key(&pair.price_asset)
     }
 
-    async fn load(&self, pair: &ExchangePair) -> ApiResult<()> {
-        let assets = [pair.amount_asset.as_str(), pair.price_asset.as_str()];
+    async fn load(&self, pair: &ExchangePair) -> Result<(), Error> {
+        let pair_assets = [pair.amount_asset.as_str(), pair.price_asset.as_str()];
 
         let resp = self
             .asset_service
-            .get(assets, None, OutputFormat::Full, false)
-            .await?;
+            .get(pair_assets, None, OutputFormat::Full, false)
+            .await
+            .map_err(|api_error| {
+                Error::AssetServiceClientError(format!(
+                    "failed to query decimals from asset-service for assets {} and {} with {:?}",
+                    pair.amount_asset, pair.price_asset, api_error,
+                ))
+            })?;
 
         let mut assets = self.asset_decimals.write().unwrap();
 
-        for asset in resp.data {
-            let Some(AssetInfo::Full(f)) = asset.data else {
-                panic!("bad AssetInfo from asset-service: {:?}", asset);
-            };
-            assert!(
-                f.precision >= 0 && f.precision <= 30,
-                "probably bad precision value {} for asset {}",
-                f.precision,
-                f.id
-            );
-            assets.insert(f.id, f.precision as u8);
-        }
+        let non_existent_assets = resp
+            .data
+            .into_iter()
+            .zip(pair_assets)
+            .filter_map(|(asset, asset_id)| {
+                let Some(AssetInfo::Full(f)) = asset.data else {
+                    return Some(asset_id);
+                };
+                assert!(
+                    f.precision >= 0 && f.precision <= 30,
+                    "probably bad precision value {} for asset {}",
+                    f.precision,
+                    f.id
+                );
+                assets.insert(f.id, f.precision as u8);
+                None
+            })
+            .collect_vec();
 
-        Ok(())
+        if non_existent_assets.is_empty() {
+            Ok(())
+        } else {
+            Err(Error::AssetServiceClientError(format!(
+                "requested assets do not exist: {:?}",
+                non_existent_assets
+            )))
+        }
     }
 
     pub fn decimals_for_asset(&self, asset: &str) -> Option<u8> {
