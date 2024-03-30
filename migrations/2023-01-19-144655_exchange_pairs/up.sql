94abd72466edb99bf58b8d2c4745e669daa28a26
Alexander Tarasenko <tarasenkoas@gmail.com>
2024-03-30 16:18:20 +0300

    CREATE INDEX CONCURRENTLY cannot run inside a transaction block

--- migrations/2023-01-19-144655_exchange_pairs/up.sql
+++ migrations/2023-01-19-144655_exchange_pairs/up.sql
@@ -1,2 +1,2 @@
-create index CONCURRENTLY if not exists "blocks_microblocks_time_stamp_uid" on blocks_microblocks(time_stamp) include(uid);
-create index CONCURRENTLY if not exists "ex_transactions_block_uid_amount_asset_price_asset_part" on transactions(exchange_amount_asset, exchange_price_asset, block_uid) where tx_type =7;
+create index if not exists "blocks_microblocks_time_stamp_uid" on blocks_microblocks(time_stamp) include(uid);
+create index if not exists "ex_transactions_block_uid_amount_asset_price_asset_part" on transactions(exchange_amount_asset, exchange_price_asset, block_uid) where tx_type =7;
